798813a044d4964cf32521b29b21edd5b0fdd4ee
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-01-27 18:32:39 +0500

    moved db config to env vars

--- scripts/minio.linux.sh
+++ scripts/minio.linux.sh
@@ -3,4 +3,4 @@ docker run \
   -p 9000:9000 \                             # Maps port 2100 on the host to 9000 in the container
   -p 9001:9001 \                             # Maps port 2101 on the host to 9001 in the container
   quay.io/minio/minio \                      # Specifies the MinIO Docker image
-  server /data --console-address ":9001"     # Configures MinIO server and management console
+  server /files --console-address ":9001"     # Configures MinIO server and management console
