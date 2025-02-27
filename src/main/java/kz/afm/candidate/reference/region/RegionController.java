a63ca25990359822a930ca27af6f3e53b99692a9
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-02-27 17:57:50 +0500

    region controller refactoring

--- src/main/java/kz/afm/candidate/reference/region/RegionController.java
+++ src/main/java/kz/afm/candidate/reference/region/RegionController.java
@@ -1,7 +1,7 @@
 package kz.afm.candidate.reference.region;
 
 import kz.afm.candidate.dto.ResponseBodyWrapper;
-import kz.afm.candidate.reference.region.dto.RegionResponse;
+import kz.afm.candidate.reference.region.dto.RegionResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +18,11 @@ public class RegionController {
     private final RegionService regionService;
 
     @GetMapping("all")
-    public ResponseEntity<ResponseBodyWrapper<List<RegionResponse>>> getAll() {
+    public ResponseEntity<ResponseBodyWrapper<List<RegionResponseDto>>> getAll() {
         try {
-            final List<RegionResponse> regions = RegionResponse.fromEntities(this.regionService.getAll());
-            return ResponseEntity.ok(ResponseBodyWrapper.success(regions));
+            final List<RegionEntity> regions = this.regionService.getAll();
+            final List<RegionResponseDto> regionDtoList = regions.stream().map(RegionResponseDto::new).toList();
+            return ResponseEntity.ok(ResponseBodyWrapper.success(regionDtoList));
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(ResponseBodyWrapper.error("Ошибка сервера"));
         }
---
+++ src/main/java/kz/afm/candidate/reference/region/dto/RegionResponseDto.java
@@ -0,0 +1,19 @@
+package kz.afm.candidate.reference.region.dto;
+
+import kz.afm.candidate.reference.region.RegionEntity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegionResponseDto {
+    public int id;
+    public String nameRus;
+    public String nameKaz;
+
+    public RegionResponseDto(RegionEntity region) {
+        this.id = region.getId();
+        this.nameRus = region.getNameRus();
+    }
+
+}
